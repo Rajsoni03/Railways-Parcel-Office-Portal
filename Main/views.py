da10c8d087ff01fa71a11c7b1e6e8fded554c30c
Raj <rajksoni029@gmail.com>
2020-01-09 21:22:12 +0530

    update

--- Main/views.py
+++ Main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'User/login.html')
+    params = {
+        'title' : 'Home'
+    }
+    return render(request, 'Main/index.html', params)
