18f9ab0a9b027823acd8a3ea3a76dccf0d78b787
rajkurmipilot992 <59687498+rajkurmipilot992@users.noreply.github.com>
2020-01-11 00:34:51 +0530

    login page created

--- Main/urls.py
+++ Main/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('train', views.train),
-    path('about/<int:no>', views.about)
+    path('about/<int:no>', views.about),
+    path('login', views.login),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def index(request):
@@ -18,3 +20,18 @@ def about(request,no):
         'about_page_no': no
     }
     return render(request, 'Main/about.html', params)
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'Main/about.html')
+        else:
+            messages.info(request, "USERNAME OR PASSWORD DID NOT MATCH !!!")
+            return redirect(login)
+    else:
+        return render(request, "Main/login.html")
